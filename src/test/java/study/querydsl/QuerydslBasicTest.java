32a238f5bf6ba8648c890415c43bf9ac138dcd86
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2022-02-23 22:03:24 +0900

    페이징

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -128,4 +128,21 @@ public class QuerydslBasicTest {
 		assertThat(member6.getUsername()).isEqualTo("member6");
 		assertThat(memberNull.getUsername()).isNull();
 	}
+	
+	@Test
+	public void paging1() {
+		QueryResults<Member> queryResults = queryFactory
+										.selectFrom(member)
+										.orderBy(member.username.desc())
+										.offset(1)
+										.limit(2)
+										.fetchResults();
+		
+		//assertThat(results.size()).isEqualTo(2);
+		
+		assertThat(queryResults.getTotal()).isEqualTo(4);
+		assertThat(queryResults.getLimit()).isEqualTo(2);
+		assertThat(queryResults.getOffset()).isEqualTo(1);
+		assertThat(queryResults.getResults().size()).isEqualTo(2);
+	}
 }
