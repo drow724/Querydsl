509a05ed654facf0cf84a157e158a852f973d23c
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2022-03-01 18:49:49 +0900

    동적 쿼리와 성능 최적화 조회 - Where절 파라미터 사용

--- src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -6,10 +6,15 @@ import static study.querydsl.entity.QTeam.team;
 
 import java.util.List;
 import java.util.Optional;
+
 import javax.persistence.EntityManager;
+
 import org.springframework.stereotype.Repository;
+
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+
 import lombok.RequiredArgsConstructor;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
@@ -53,9 +58,9 @@ public class MemberJpaRepository {
 	// Builder 사용
 	// 회원명, 팀명, 나이(ageGoe, ageLoe)
 	public List<MemberTeamDto> searchByBuilder(MemberSearchCondition condition) {
-		
+
 		BooleanBuilder builder = new BooleanBuilder();
-		
+
 		if (hasText(condition.getUsername())) {
 			builder.and(member.username.eq(condition.getUsername()));
 		}
@@ -68,8 +73,44 @@ public class MemberJpaRepository {
 		if (condition.getAgeLoe() != null) {
 			builder.and(member.age.loe(condition.getAgeLoe()));
 		}
-		
+
 		return queryFactory.select(new QMemberTeamDto(member.id, member.username, member.age, team.id, team.name))
 				.from(member).leftJoin(member.team, team).where(builder).fetch();
+	}
+
+	// 회원명, 팀명, 나이(ageGoe, ageLoe)
+	public List<MemberTeamDto> search(MemberSearchCondition condition) {
+		return queryFactory.select(new QMemberTeamDto(member.id, member.username, member.age, team.id, team.name))
+				.from(member).leftJoin(member.team, team)
+				.where(usernameEq(condition.getUsername()), teamNameEq(condition.getTeamName()),
+						ageGoe(condition.getAgeGoe()), ageLoe(condition.getAgeLoe()))
+				.fetch();
+	}
+
+	private BooleanExpression usernameEq(String username) {
+		return hasText(username) ? member.username.eq(username) : null;
+	}
+
+	private BooleanExpression teamNameEq(String teamName) {
+		return hasText(teamName) ? team.name.eq(teamName) : null;
+	}
+
+	private BooleanExpression ageGoe(Integer ageGoe) {
+		return ageGoe != null ? member.age.goe(ageGoe) : null;
+	}
+
+	private BooleanExpression ageLoe(Integer ageLoe) {
+		return ageLoe != null ? member.age.loe(ageLoe) : null;
+	}
+
+	private BooleanExpression ageBetween(Integer ageLoe,Integer ageGoe) {
+		return ageGoe(ageGoe).and(ageLoe(ageLoe));
+	}
+	
+	// where 파라미터 방식은 이런식으로 재사용이 가능하다.
+	public List<Member> findMember(MemberSearchCondition condition) {
+		return queryFactory.selectFrom(member).leftJoin(member.team, team).where(usernameEq(condition.getUsername()),
+				teamNameEq(condition.getTeamName()), ageBetween(condition.getAgeLoe(), condition.getAgeGoe()))
+				.fetch();
 	}
 }
